397f50ae39652220b4ce53f35e5f24d41e23422c
karthikjaganreddy18 <karthikreddy41182@gmail.com>
2024-10-02 21:48:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotEnv = require('dotenv');
+const app = express()
+const mongoose = require('mongoose');
+const PORT = 4000;
+dotEnv.config();
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("Mongodb connected successfully"))
+.catch((error)=>console.log(error))
+
+
+app.listen(PORT, ()=>{
+    console.log(`server started and running at ${PORT}`);
+})
+app.use('/home',(req,res)=>{
+    res.send("<h1>Welcome to website")
+})
+
+
